634b8799c1c7442ada22c2d2d1456b493c06c6dd
Mauri44444 <mauri14200@gmail.com>
2024-06-16 00:08:41 -0300

    Add files via upload

---
+++ AÑO BICIESTO IV/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AÑO_BICIESTO_IV;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, ingrese un año: ");
+        int year;
+        
+        
+        if (int.TryParse(Console.ReadLine(), out year))             // Se intenta convertir la entrada del usuario en un numero entero
+        {
+            
+            if (EsAñoBiciesto(year))             // Se determina si el año es bisiesto
+            {
+                Console.WriteLine($"El año {year} es un año bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} no es un año bisiesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("La entrada no es un número válido.");
+        }
+    }
+
+    static bool EsAñoBiciesto(int year)   // Usamos un metodo booleano para ver si el año cumple las condiciones para ser bisiesto
+    {
+        return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);         // El año es bisiesto si es divisible por 4. Excepto si es divisible por 100, pero no por 400
+    }
+}
+
+
+
---
+++ PALINDROMO V/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PALINDROMO_V;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, ingrese una cadena de texto: ");
+        string input = Console.ReadLine();
+        
+        // Eliminar espacios en blanco y convertir a minúsculas
+        string processedInput = input.Replace(" ", "").ToLower();
+        
+        // Verificar si la cadena es un palíndromo
+        if (IsPalindrome(processedInput))
+        {
+            Console.WriteLine("La cadena ingresada es un palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine("La cadena ingresada no es un palíndromo.");
+        }
+    }
+
+    static bool IsPalindrome(string text)
+    {
+        int left = 0;
+        int right = text.Length - 1;
+
+        while (left < right)
+        {
+            if (text[left] != text[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
